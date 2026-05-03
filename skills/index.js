0ad1c87b4296ad31b4b24a365742ad2e10460e95
Cult of Psyche Official <kultofpsyche@gmail.com>
2026-05-02 21:50:04 -0700

    Refactor loadSkills function for directory path

--- skills/index.js
+++ skills/index.js
@@ -1,17 +1,17 @@
 const fs = require("fs");
 const path = require("path");
 
-const skillsDir = path.join(__dirname);
-
 function loadSkills() {
-  const files = fs.readdirSync(skillsDir);
+  const dir = path.join(__dirname);
+
+  const files = fs.readdirSync(dir);
 
   let skills = {};
 
   for (let file of files) {
     if (file !== "index.js" && file.endsWith(".js")) {
-      const skillName = file.replace(".js", "");
-      skills[skillName] = require(`./${file}`);
+      const name = file.replace(".js", "");
+      skills[name] = require(`./${file}`);
     }
   }
 
